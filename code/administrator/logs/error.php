5cec4b49995036df260d7db5ea176ba99817c032
Krzysztof Olszewski <hello@retryjoin.pl>
2024-04-22 07:09:55 +0200

    running

---
+++ code/administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-04-21 20:16:50 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-04-21T20:16:50+00:00	INFO 172.22.0.2	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-04-21T20:17:00+00:00	INFO 172.22.0.2	joomlafailure	Username and password do not match or you do not have an account yet.
